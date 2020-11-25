308ffb5681c4de7660876c63c50f859f18fd1191
jane baik <janebaik@berkeley.edu>
2020-11-25 12:11:53 -0800

    finished association

--- skeleton/app/models/user.rb
+++ skeleton/app/models/user.rb
@@ -15,5 +15,13 @@ class User < ApplicationRecord
         primary_key: :id,
         foreign_key: :student_id,
         class_name: :Enrollment
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course
+
+        # has_many :posts,
+        # through: :users, #if line 13 said :banana instead, this would be :banana
+        # source: :posts
    
 end
