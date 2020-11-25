e08106362f6a47c05e8d04991b2b5e2364526abe
jane baik <janebaik@berkeley.edu>
2020-11-25 13:39:03 -0800

    at shortenedurl

---
+++ URLShortener/app/models/user.rb
@@ -0,0 +1,19 @@
+# class User < ApplicationRecord
+#     validates :username, presence: true, uniqueness: true
+
+#     has_many :posts,
+#         class_name: :Post,
+#         foreign_key: :author_id,
+#         primary_key: :id
+
+#     belongs_to :location,
+#         primary_key: :id,
+#         foreign_key: :location_id,
+#         class_name: :Location
+# end
+
+class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: true
+
+
+end
